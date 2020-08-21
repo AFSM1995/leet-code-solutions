5abcbf1c6e33dd6d38f9cd8aa9ef6f00619f9e2e
Alvaro Santillan <afsm1995@live.com>
2020-08-21 14:47:58 -0600

    Runtime: 24 ms, faster than 27.89% of Swift online submissions for Sqrt(x). Memory Usage: 20.5 MB, less than 63.69% of Swift online submissions for Sqrt(x).

---
+++ easy/27.89run 63.69mem sqrtx.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Álvaro Santillan on 8/21/20.
+//  Copyright © 2020 Álvaro Santillan. All rights reserved.
+//  https://leetcode.com/problems/sqrtx/submissions/
+//
+
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        return Int(sqrt(Double(x)))
+    }
+}

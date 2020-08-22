f16efe6e540396ad7517ea5065600155d20ac1f7
Alvaro Santillan <afsm1995@live.com>
2020-08-21 19:25:19 -0600

    Added Whiteboard "PlayGround"

---
+++ whiteboard/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  whiteboard
+//
+//  Created by Álvaro Santillan on 8/21/20.
+//  Copyright © 2020 Álvaro Santillan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
